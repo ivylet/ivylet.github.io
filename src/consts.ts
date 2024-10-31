0fdd598f3a39c41d0694dc833c01456205cdbca8
saytaytay <2904026804@qq.com>
2024-10-31 19:59:56 +0800

    upload posts

--- src/consts.ts
+++ src/consts.ts
@@ -13,7 +13,7 @@ export type Link = {
 }
 
 export const SITE: Site = {
-  TITLE: 'astro-erudite',
+  TITLE: 'freedom',
   DESCRIPTION:
     'astro-erudite is a opinionated, no-frills blogging template—built with Astro, Tailwind, and shadcn/ui.',
   EMAIL: 'jason@enscribe.dev',
